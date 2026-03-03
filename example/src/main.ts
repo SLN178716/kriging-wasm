e68400a5a2c6d0c9f07d8a91a0f2bc174bf5185a
史连宁 <shilianning@yuanxinjituan.com>
2026-03-03 10:19:53 +0800

    feat: commit

--- example/src/main.ts
+++ example/src/main.ts
@@ -4,7 +4,7 @@ import kriging from '@sakitam-gis/kriging';
 import { colors, type releaseType } from './constant';
 import * as turf from '@turf/turf';
 import './main.css';
-import initKrigingWasm, { kriging_interpolate } from './kriging-wasm/kriging_wasm'
+import initKrigingWasm, { interpolate_grid, type VariogramModel } from './kriging-wasm/kriging_wasm'
 
 
 const tdt_key = '36294b070b7f84a954582df42f1b6cb1'
@@ -54,25 +54,15 @@ const generateData = () => {
   const positionData = turf.randomPoint(50, { bbox: turf.bbox(boundaries) });
   // 再生成些随机数做属性
   turf.featureEach(positionData, function (currentFeature) {
-    currentFeature.properties = { value: (Math.random() * 100).toFixed(2) };
+    currentFeature.properties = { value: Number((Math.random() * 100).toFixed(2)) };
   });
   return { boundaries, positionData }
 }
 
 const showKriging = (type: releaseType) => {
-  if (type !== 'js') {
-    initKrigingWasm().then(() => {
-      kriging_interpolate({
-        points: [{
-          x: 110,
-          y: 32
-        }]
-      })
-    });
-    return
-  }
   // 清空图层
   clearKriging();
+
   // 完全透明
   const scope = L.geoJSON(boundaries, {
     style: function () {
@@ -87,14 +77,31 @@ const showKriging = (type: releaseType) => {
   }).addTo(imageLayerGroup!);
 
   map!.fitBounds(scope.getBounds());
-  //根据scope边界线，生成范围信息
-  const xlim = [scope.getBounds().getSouthWest().lng, scope.getBounds().getNorthEast().lng];
-  const ylim = [scope.getBounds().getSouthWest().lat, scope.getBounds().getNorthEast().lat];
 
   const canvas = document.createElement('canvas');
   canvas.width = 2000;
   canvas.height = 1000;
-  function loadkriging() {
+
+  // 将插值范围封装成特定格式
+  const bbox = turf.bbox(boundaries); // 外包矩形范围
+  // 根据外包矩形范围生成外包矩形面Polygon
+  const bboxPolygon = turf.bboxPolygon(bbox);
+  const range: [number, number][][] = bboxPolygon.geometry.coordinates.map(coordinate => {
+    return coordinate.map(v => [v[0], v[1]])
+  })
+  
+  // 克里金插值参数
+  const params = {
+    krigingModel: 'Exponential',//model还可选'gaussian','spherical'
+    krigingSigma2: 0,
+    krigingAlpha: 100,
+    interval: 0.05, // 插值格点精度大小
+    canvasAlpha: 0.8,//canvas图层透明度-0.75
+    colors
+  }
+
+  function jsKriging() {
+    performance.mark('js start');
     const points = positionData
     // 数量
     const pointLength = points.features.length;
@@ -108,39 +115,86 @@ const showKriging = (type: releaseType) => {
       t.push(points.features[i].properties.value);
     }
 
-    // 克里金插值参数
-    const params = {
-      krigingModel: 'exponential',//model还可选'gaussian','spherical'
-      krigingSigma2: 0,
-      krigingAlpha: 100,
-      canvasAlpha: 0.8,//canvas图层透明度-0.75
-      colors
-    }
     // 对数据集进行训练
-    const variogram = kriging.train(t, x, y, params.krigingModel, params.krigingSigma2, params.krigingAlpha);
-    // 将插值范围封装成特定格式
-    const bbox = turf.bbox(boundaries); // 外包矩形范围
-    // 根据外包矩形范围生成外包矩形面Polygon
-    const bboxPolygon = turf.bboxPolygon(bbox);
-    const positions: number[][] = [];
-    bboxPolygon.geometry.coordinates[0].forEach((v) => {
-      positions.push([v[0], v[1]])
-    })
-    // 将边界封装成特定的格式
-    const range = [positions]
+    const variogram = kriging.train(t, x, y, params.krigingModel.toLowerCase(), params.krigingSigma2, params.krigingAlpha);
+    
     // 使用variogram对象使polygons描述的地理位置内的格网元素具备不一样的预测值,最后一个参数，是插值格点精度大小
-    const grid = kriging.grid(range, variogram, 0.05);
+    const grid = kriging.grid(range, variogram, params.interval);
+    console.log(grid!.data.reduce((acc, cur) => {
+      const val = cur.filter((v: unknown) => v !== null && v !== undefined).length
+      return acc + val
+    }, 0))
     // 将得到的格网grid渲染至canvas上
-    kriging.plot(canvas, grid!, xlim, ylim, params.colors);
+    kriging.plot(canvas, grid!, grid!.xlim, grid!.ylim, params.colors);
+    
+    const imageBounds: L.LatLngBoundsExpression = range[0].map(v => [v[1], v[0]]);
+    L.imageOverlay(canvas.toDataURL("image/png"), imageBounds, { opacity: 0.8 }).addTo(imageLayerGroup!);
+    performance.mark('js end');
+    console.log(performance.measure('jsKriging', 'js start', 'js end'));
+  }
+
+  function wasmKriging() {
+    performance.mark('wasm start');
+    const zlim = [positionData.features[0].properties.value, positionData.features[0].properties.value];
+    const { points, xlim, ylim } = interpolate_grid({ 
+      base: {
+        kriging_type: 'Ordinary',
+        model_type: params.krigingModel as VariogramModel,
+        nugget: params.krigingSigma2,
+        range: params.krigingAlpha,
+        sill: null
+      },
+      known_points: positionData.features.map(feature => {
+        zlim[0] = Math.min(zlim[0], feature.properties.value);
+        zlim[1] = Math.max(zlim[1], feature.properties.value);
+        return {
+          point: feature.geometry.coordinates as [number, number],
+          value: feature.properties.value,
+        }
+      }),
+      polygons: range,
+      interval: params.interval,
+    })
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      const range = [xlim[1] - xlim[0], ylim[1] - ylim[0], zlim[1] - zlim[0]];
+      const wx = Math.ceil(params.interval * canvas.width / (xlim[1] - xlim[0]));
+      const wy = Math.ceil(params.interval * canvas.height / (ylim[1] - ylim[0]));
+      for (const point of points) {
+        const x = canvas.width * (point.point[0] - xlim[0]) / range[0];
+        const y = canvas.height * (1 - (point.point[1] - ylim[0]) / range[1]);
+        let z = (point.value - zlim[0]) / range[2];
+        if (z < 0.0) z = 0.0;
+        if (z > 1.0) z = 1.0;
+        ctx.fillStyle = colors[Math.floor((colors.length - 1) * z)];
+        ctx.fillRect(Math.round(x - wx / 2), Math.round(y - wy / 2), wx, wy);
+      }
+    }
+    const imageBounds: L.LatLngBoundsExpression = range[0].map(v => [v[1], v[0]]);;
+    L.imageOverlay(canvas.toDataURL("image/png"), imageBounds, { opacity: 0.8 }).addTo(imageLayerGroup!);
+    
+    performance.mark('wasm end');
+    console.log(performance.measure('wasmKriging', 'wasm start', 'wasm end'));
   }
 
   // 执行克里金插值函数
-  loadkriging();
-
-  const imageBounds: L.LatLngBoundsExpression = [[ylim[0], xlim[0]], [ylim[1], xlim[1]]];
-  L.imageOverlay(canvas.toDataURL("image/png"), imageBounds, { opacity: 0.8 }).addTo(imageLayerGroup!);
+  if (type === 'wasm') {
+    wasmKriging()
+  } else {
+    jsKriging();
+  }
 }
 
-initMap()
+declare global {
+  interface Window {
+    showKriging: (type: releaseType) => void;
+  }
+}
+
 const { boundaries, positionData } = generateData()
-showKriging('wasm')
+
+initKrigingWasm().then(() => {
+  initMap()
+  window.showKriging = showKriging
+})
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,6 +2,8 @@ use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use tsify_next::Tsify;
 use oxigdal_analytics::interpolation::kriging;
+use ndarray::{Array2, ArrayView, ArrayView1};
+use web_sys::console;
 
 #[derive(Serialize, Deserialize, Debug, Clone, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
@@ -22,56 +24,215 @@ pub enum VariogramModel {
 // 输入数据结构
 #[derive(Serialize, Deserialize, Debug, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct KrigingInput {
+#[derive(Clone)]
+pub struct KrigingOption {
     pub kriging_type: KrigingType,
     pub model_type: VariogramModel,
-    pub nugget: Option<f64>,
-    pub sill: Option<f64>,
-    pub range: Option<f64>,
-    pub points: Vec<Point>,
-    pub target_points: Vec<Point>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Tsify)]
-#[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct Point {
-    pub x: f64,
-    pub y: f64,
-    pub value: f64,
-}
-
-// 输出数据结构
-#[derive(Serialize, Deserialize, Debug, Tsify)]
-#[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct KrigingOutput {
-    pub predictions: Vec<Prediction>,
-    pub variogram_model: VariogramModelInfo,
-    pub mse: Option<f64>, // 均方误差
-}
-
-#[derive(Serialize, Deserialize, Debug, Tsify)]
-#[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct Prediction {
-    pub x: f64,
-    pub y: f64,
-    pub value: f64,
-    pub variance: f64, // 克里金方差
-}
-
-#[derive(Serialize, Deserialize, Debug, Tsify)]
-#[tsify(into_wasm_abi, from_wasm_abi)]
-pub struct VariogramModelInfo {
-    pub model_type: String,
     pub nugget: f64,
-    pub sill: f64,
     pub range: f64,
+    pub sill: Option<f64>,
+}
+
+// 输入数据结构
+#[derive(Serialize, Deserialize, Debug, Tsify)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+#[derive(Clone)]
+pub struct KnownPoint {
+    pub point: (f64, f64),
+    pub value: f64,
+}
+
+// 输出数据点结构
+#[derive(Serialize, Deserialize, Debug, Tsify)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct ResultPoint {
+    pub point: (f64, f64),
+    pub value: f64,
+    pub variance: f64,
+}
+
+// 输出网格数据结构
+#[derive(Serialize, Deserialize, Debug, Tsify)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct InterpolateGridResult {
+    pub points: Vec<ResultPoint>,
+    pub xlim: (f64, f64),
+    pub ylim: (f64, f64),
+}
+
+#[derive(Serialize, Deserialize, Debug, Tsify)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct InterpolatePointsOption {
+    pub base: KrigingOption,
+    pub known_points: Vec<KnownPoint>,
+    pub target_points: Vec<(f64, f64)>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Tsify)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct InterpolateGridOption {
+    pub base: KrigingOption,
+    pub known_points: Vec<KnownPoint>,
+    pub polygons: Vec<Vec<(f64, f64)>>,
+    pub interval: f64,
 }
 
 // 将 Rust 结构转换为 JavaScript 对象
 #[wasm_bindgen]
-pub fn kriging_interpolate(input: &KrigingInput) -> String {
-    println!("input: {:?}", input);
-    return "{}".to_string()
+pub fn interpolate_points(input: &InterpolatePointsOption) -> Result<Vec<ResultPoint>, JsError> {
+    // 准备数据
+    let points: Vec<f64> = input.known_points.iter().flat_map(|p| vec![p.point.0, p.point.1]).collect();
+    let points: Array2<f64> = Array2::from_shape_vec((input.known_points.len(), 2), points).unwrap();
+    let values: Vec<f64> = input.known_points.iter().map(|p| p.value).collect();
+    let values: ArrayView1<f64> = ArrayView::from(values.as_slice());
+    let target_points: Vec<f64> = input.target_points.iter().flat_map(|p| vec![p.0, p.1]).collect();
+    let target_points: Array2<f64> = Array2::from_shape_vec((input.target_points.len(), 2), target_points).unwrap();
+    let base_option = &input.base;
+    // 创建克里金插值器
+    let kriging_type = match base_option.kriging_type {
+        KrigingType::Ordinary => kriging::KrigingType::Ordinary,
+        KrigingType::Universal => kriging::KrigingType::Universal,
+    };
+    
+    // 设置变异函数模型
+    let model = match base_option.model_type {
+        VariogramModel::Spherical => kriging::VariogramModel::Spherical,
+        VariogramModel::Exponential => kriging::VariogramModel::Exponential,
+        VariogramModel::Gaussian => kriging::VariogramModel::Gaussian,
+        VariogramModel::Linear => kriging::VariogramModel::Linear,
+    };
+
+    let var = values.var(0.0);
+
+    let variogram = kriging::Variogram::new(model, base_option.nugget, base_option.sill.unwrap_or(var), base_option.range);
+
+    let interpolator = kriging::KrigingInterpolator::new(kriging_type, variogram);
+    
+    // 执行插值
+    match interpolator.interpolate(&points, &values, &target_points) {
+        Ok(result) => {
+            let result_points: Vec<ResultPoint> = result.values.into_iter().enumerate().map(|(i, v)| {
+                ResultPoint {
+                    point: (result.coordinates[[i, 0]], result.coordinates[[i, 1]]),
+                    value: v,
+                    variance: result.variances[i],
+                }
+            }).collect();
+            
+            Ok(result_points)
+        },
+        Err(e) => Err(JsError::new(&e.to_string())),
+    }
+}
+
+#[wasm_bindgen]
+pub fn interpolate_grid(input: &InterpolateGridOption) -> Result<InterpolateGridResult, JsError> {
+    let (target_points, xlim, ylim) = grid2points(&input.polygons, input.interval);
+    
+    let result_points = interpolate_points(&InterpolatePointsOption {
+        base: input.base.clone(),
+        known_points: input.known_points.clone(),
+        target_points,
+    })?;
+    
+    Ok(InterpolateGridResult {
+        points: result_points,
+        xlim: (xlim.0, xlim.1),
+        ylim: (ylim.0, ylim.1),
+    })
+}
+
+fn grid2points(polygons: &Vec<Vec<(f64, f64)>>, interval: f64) -> (Vec<(f64, f64)>, (f64, f64), (f64, f64)) {
+    let n = polygons.len();
+    // 至少需要 1 个多边形来定义一个区域
+    if n < 1 {
+        return (Vec::new(), (0.0, 0.0), (0.0, 0.0));
+    }
+
+    // 计算所有多边形的边界范围
+    let mut xlim = (polygons[0][0].0, polygons[0][0].0);
+    let mut ylim = (polygons[0][0].1, polygons[0][0].1);
+
+    let mut lims: Vec<((f64, f64), (f64, f64))> = Vec::new();
+    for polygon in polygons {
+        let mut xy = ((polygon[0].0, polygon[0].0), (polygon[0].1, polygon[0].1));
+        for point in polygon {
+            xlim.0 = xlim.0.min(point.0);
+            xlim.1 = xlim.1.max(point.0);
+            ylim.0 = ylim.0.min(point.1);
+            ylim.1 = ylim.1.max(point.1);
+            xy.0.0 = xy.0.0.min(point.0);
+            xy.0.1 = xy.0.1.max(point.0);
+            xy.1.0 = xy.1.0.min(point.1);
+            xy.1.1 = xy.1.1.max(point.1);
+        }
+        lims.push(xy);
+    }
+
+    let mut points = Vec::new();
+    for (idx, polygon) in polygons.iter().enumerate() {
+        let (lxlim, lylim) = lims[idx];
+        let mut i = xlim.0;
+        while i < xlim.1 {
+            if i < lxlim.0 {
+                i += interval;
+                continue;
+            }
+            if i > lxlim.1 {
+                break;
+            }
+            let mut j = ylim.0;
+            while j < ylim.1 {
+                if j < lylim.0 {
+                    j += interval;
+                    continue;
+                }
+                if j > lylim.1 {
+                    break;
+                }
+                let p = (i, j);
+                if point_in_polygon(&p, &polygon) {
+                    points.push(p);
+                }
+                j += interval;
+            }
+            i += interval;
+        }
+    }
+    (points, xlim, ylim)
+}
+
+fn point_in_polygon(point: &(f64, f64), polygon: &Vec<(f64, f64)>) -> bool {
+    if polygon.len() < 3 {
+        return false;
+    }
+
+    let (x, y) = *point;
+    let mut inside = false;
+    let n = polygon.len();
+
+    for i in 0..n {
+        let j = (i + 1) % n;
+        let (xi, yi) = polygon[i];
+        let (xj, yj) = polygon[j];
+
+        // 检查点是否在顶点上
+        if (x == xi && y == yi) || (x == xj && y == yj) {
+            return true;
+        }
+
+        // 检查点的 y 坐标是否在边的 y 范围内
+        if (yi > y) != (yj > y) {
+            // 计算交点的 x 坐标
+            let x_intersect = ((y - yi) * (xj - xi)) / (yj - yi) + xi;
+            // 如果交点在点的右侧，则切换 inside 状态
+            if x < x_intersect {
+                inside = !inside;
+            }
+        }
+    }
+
+    inside
 }
 
 // 版本信息
